76789b1dfdd7f81d5a87d24b0187935f34a64e99
DireCorgi <s.frank.ye@gmail.com>
2016-12-20 12:20:04 -0500

    fix demo entry file

--- demo.rb
+++ demo.rb
@@ -9,17 +9,28 @@ DEMO_SQL = 'farms.sql'
 DBConnection.open(DEMO_DB)
 
 class Chicken < SQLObject
+  belongs_to :owner, class_name: 'Farmer'
+
   finalize!
 end
 
 class Cow < SQLObject
+  belongs_to :owner, class_name: 'Farmer'
+
   finalize!
 end
 
 class Farmer < SQLObject
+  has_many :cows, foreign_key: :owner_id
+  has_many :chickens, foreign_key: :owner_id
+
+  belongs_to :farm
+
   finalize!
 end
 
 class Farm < SQLObject
+  has_many :farmers
+
   finalize!
 end
